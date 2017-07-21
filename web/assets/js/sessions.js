bb2394bb7ce0210d0f3f60e5fad4a7e7faf2738f
Aurore Gaucher <gaucher.aurore@laposte.net>
2017-07-21 19:56:02 +0200

    rename sessions.js file

---
+++ web/assets/js/sessions.js
@@ -0,0 +1,22 @@
+function showDatePicker() {
+    $('.input-group.date').datepicker({
+            format: "dd/mm/yyyy",
+            weekStart: 1,
+            maxViewMode: 2,
+            todayBtn: "linked",
+            language: "fr",
+            todayHighlight: true,
+            autoclose: true
+        }
+    );
+}
+
+function getPrice() {
+    if (($('#session_noshow').is(':checked'))) {
+        $('#session_price').val(selectedProject.price / 2);
+    } else {
+        $('#session_price').val(selectedProject.price);
+    }
+
+}
+
